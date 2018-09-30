bb0e20abc0d5e311983c6dd4fc82df4936ff713d
qihuawan <qihuawan@shein.com>
2018-09-30 17:21:46 +0800

    0930 list 滑动删除

--- src/components/input.vue
+++ src/components/input.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <!--<div class="test">TODO</div>-->
+    <div class="test">test</div>
     <div class="input_box">
       <textarea class="input_todo" rows="3" placeholder="please input ..." v-model="newItem"></textarea>
       <div class="enter_input" @click="save">保存</div>
     </div>
     <div class="todo_box">
-      <div v-for="item in todoList">
+      <div class="sub_item" v-for="item in todoList" ref="touch" v-touch :key="item.id">
         <div class="had_done" :class="{'done_btn':item.done===true}" @click="doneEvent(item.tiemId)"></div>
-        <p  :class="[ item.done===true ? 'done todo_time_done' : 'todo_time' ] " >截止时间 {{item.deadline}} </p>
+        <p  :class="[ item.done===true ? 'done todo_time_done' : 'todo_time' ] " >deadline ~ {{item.deadline}} </p>
         <p class="todo_detail" :class="{'done':item.done===true}">{{item.todoItem}}</p>
+        <div class="delete_btn">删除</div>
       </div>
     </div>
   </div>
@@ -20,8 +21,8 @@
   export default {
     data(){
       return {
-        todoList:[
-          /*{
+        todoList: [
+          {
             "tiemId": "2354sdfh23",
             "done": true,
             "deadline": "18:00",
@@ -38,9 +39,10 @@
             "done": false,
             "deadline": "18:00",
             "todoItem": "今天下午需要把相关的需求文档熟悉一遍并完成60%的相关功能",
-          }*/
+          }
         ],
-        newItem:'',
+        newItem: '',
+        expansion: null,
       }
     },
     methods:{
@@ -49,7 +51,7 @@
       },
       // 保存
       save(){
-        if( this.trim(this.newItem).length > 0  ){
+        if( this.trim(this.newItem).length > 0 ){
           let newList = {};
           newList.tiemId = createCode();
           newList.done = false;
@@ -70,9 +72,94 @@
             }
           }
         }
-      }
+      },
     },
     components:{
+    },
+    directives:{
+      touch: {
+        bind: function(el){
+          let initX;                 // 触摸位置X
+          let initY;                 // 触摸位置Y
+          let moveX;                 // 滑动时的位置X
+          let moveY;                 // 滑动时的位置Y
+          let X;                     // 移动距离
+          let Y;                     // 移动距离
+          let objX = 0;              // 目标对象位置
+          let touchFlag = false;     // 判断是点击还是滑动
+          let em;
+          let swipeX;
+          let swipeY;
+          el.addEventListener('touchstart',function(event){
+            em = 5 * parseInt(document.defaultView.getComputedStyle(el, null).fontSize);
+            event.stopPropagation();    // 阻止冒泡
+            touchFlag = false;
+            swipeX = true;
+            swipeY = true;
+            initX = event.targetTouches[0].pageX;
+            initY = event.targetTouches[0].pageY;
+            objX = (el.style.transform.replace(/translateX\(/g, "").replace(/px\)/g, "")) * 1;
+            if( this.expansion && this.expansion !== event.currentTarget ){   //判断是否有展开，如果有展开则收起
+              this.expansion.style.transform = "translateX(0px)";
+            }
+          },false);
+          el.addEventListener('touchmove',function(event){
+            moveX = event.targetTouches[0].pageX;
+            moveY = event.targetTouches[0].pageY;
+            X = moveX - initX;
+            Y = moveY - initY;
+            if( swipeX && Math.abs(X) > Math.abs(Y) ){
+              event.stopPropagation();   // 阻止冒泡
+              swipeY = false;
+              touchFlag = true;
+              if( objX === 0 ){       // 收起的状态
+                if(X >= 0){
+                  el.style.transform = "translateX(" + 0 + "px)";
+                }else if (X < 0){
+                  if (X < -em) {
+                    X = -em;
+                  }
+                  el.style.transform = "translateX(" + X + "px)";
+                }
+              }else if( objX < 0 ){   // 展开的状态
+                if(X >= 0){
+                  if( (X - em) > 0 ){
+                    X = em;
+                  }
+                  el.style.transform = "translateX(" + (X - em) + "px)";
+                }else{                // 保持展开状态
+                  el.style.transform = "translateX(-"+ em +"px)";
+                }
+              }
+            }else if(swipeY && Math.abs(X) - Math.abs(Y) < 0){  //上下滑动
+              touchFlag = true;
+              swipeX = false;
+              //上下滑动，使用浏览器默认的上下滑动
+            }
+          },false);
+          el.addEventListener('touchend', function(event) {
+            event.stopPropagation();   // 阻止冒泡
+            if( touchFlag === false ){
+              // code for noMove
+              if( event.target.className === 'delete_btn' ){
+                console.log(event.target.className)
+                el.className += ' disappear';
+                setTimeout(function(){
+                  el.parentNode.removeChild(el);
+                },400)
+              }
+            }else{
+              objX = (el.style.transform.replace(/translateX\(/g, "").replace(/px\)/g, "")) * 1;
+              if (objX > -em/2) {
+                el.style.transform = "translateX(" + 0 + "px)";
+              } else {
+                this.expansion = el;
+                el.style.transform = "translateX(" + -em + "px)";
+              }
+            }
+          },false)
+        }
+      }
     }
   }
 </script>
@@ -81,7 +168,7 @@
   .test {
     @include font($font-size-40, 2em, $color-33, center);
     @include wh(100%, 2em);
-    margin: 1em auto .3em;
+    padding: 1em auto .3em;
   }
 
   .input_box {
@@ -130,8 +217,11 @@
     @include font($font-size-28, 2em, $color-33, left);
     border-radius:  .5em;
     background: $background-color-f8;
-    >div {
+    overflow-x: hidden;
+    .sub_item {
       @include border-dpr(border-bottom, 1px, #999);
+      box-sizing: border-box;
+      height: 4.75em;
       padding: .5em 0;
       position: relative;
       .had_done {
@@ -195,8 +285,20 @@
         white-space: nowrap;
         transition: all 1s;
       }
+      .delete_btn {
+        position: absolute;
+        top: 0;
+        right: -5em;
+        height: 100%;
+        width: 5em;
+        background: #e63838;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
     }
-    >div:last-child {
+    .sub_item:last-child {
       border-bottom: none;
     }
   }
@@ -207,7 +309,6 @@
     color: $color-99!important;
     transition: all 1s;
   }
-
 
   /*1px极细线的问题*/
   .borBot {
@@ -227,6 +328,14 @@
       -webkit-transform: scaleY(0.5);
       transform: scaleY(0.5);
     }
+  }
+
+  .disappear {
+    height: 0!important;
+    padding: 0!important;
+    overflow: hidden;
+    -webkit-transition: height .4s ease-in-out, padding .4s ease-in-out;
+            transition: height .4s ease-in-out, padding .4s ease-in-out;
   }
 
 </style>
